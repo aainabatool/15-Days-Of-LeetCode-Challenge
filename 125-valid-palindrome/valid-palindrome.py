b1ff01a1db3ecfc15f9856c27c2875de32f7e1fd
Aaina Batool <b.aaina@yahoo.com>
2024-11-25 22:32:18 +0500

    Time: 10 ms (86.04%) | Memory: 14.1 MB (19.45%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        letters = [letter.lower() for letter in s if letter.isalnum()]
+        
+        return letters == letters[::-1]
+        
